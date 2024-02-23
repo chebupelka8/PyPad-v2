504baa421e0cc98296f0179be36658869d53edb4
chebupelka <119750562+chebupelka8@users.noreply.github.com>
2024-02-23 20:45:53 +0500

    Add custom fonts (Now you can add your own fonts)

--- scr/scripts/font.py
+++ scr/scripts/font.py
@@ -1,10 +1,23 @@
 from PySide6.QtGui import QFontDatabase, QFont
 
+import os
+
 
 class Font:
+    @classmethod
+    def get_all_font_families(cls) -> list[str]:
+        system_fonts = QFontDatabase.families()
+
+        return sorted(system_fonts + cls.__get_additional_fonts())
+
     @staticmethod
-    def get_all_font_families() -> list[str]:
-        return QFontDatabase.families()
+    def __get_additional_fonts() -> list[str]:
+        res = []
+
+        for font in os.listdir("assets/fonts"):
+            res.append(font)
+
+        return res
 
     @staticmethod
     def get_font_by_path(__path: str, __size: int | float, __bold: bool = False, __italic: bool = False) -> QFont:
@@ -16,8 +29,18 @@ class Font:
 
         return font
 
-    @staticmethod
-    def get_system_font(__family: str, __size: int | float, __bold: bool = False, __italic: bool = False) -> QFont:
+    @classmethod
+    def get_system_font(
+            cls,
+            __family: str,
+            __size: int | float,
+            __bold: bool = False,
+            __italic: bool = False,
+            check_exist_additional: bool = True
+    ) -> QFont:
+        if check_exist_additional and __family in cls.__get_additional_fonts():
+            return cls.get_font_by_path(f"assets/fonts/{__family}", __size, __bold, __italic)
+
         font = QFont(__family, __size, 1, __italic)
         font.setBold(__bold)
 
