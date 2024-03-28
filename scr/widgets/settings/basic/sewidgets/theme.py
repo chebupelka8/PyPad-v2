24d1f0f698bb4e24aa1f4f65615330a84eb32d55
chebupelka <119750562+chebupelka8@users.noreply.github.com>
2024-03-28 22:04:06 +0500

    Fixed bug with subsetting 'themes'

--- scr/widgets/settings/basic/sewidgets/theme.py
+++ scr/widgets/settings/basic/sewidgets/theme.py
@@ -1,6 +1,6 @@
 from ...abstract import AbstractSettingsWidget, AbstractSettingFrame, FrameTitles
 
-from scr.interface.additional import ThemeChanger
+from scr.interface.additional import ThemeChangerWindow
 
 from scr.scripts.tools.file import FileLoader
 
@@ -11,7 +11,7 @@ class ThemeSettingsWidget(AbstractSettingsWidget):
     def __init__(self, __restarter) -> None:
         super().__init__()
 
-        self.themeChanger = ThemeChanger(self, restarter=__restarter)
+        self.themeChanger = ThemeChangerWindow(self, __restarter)
 
         self.font_theme_changer = AbstractSettingFrame("Color Theme", "Defines the current color theme")
         self.change_theme = self.font_theme_changer.add_button("Change color theme...", is_highlighted=True)
@@ -23,5 +23,5 @@ class ThemeSettingsWidget(AbstractSettingsWidget):
     def show_theme_changer(self):
         themes = [FileLoader.load_json(f"scr/data/themes/{i}")["name"] for i in os.listdir("scr/data/themes")]
 
-        self.themeChanger.set_items(*themes)
+        self.themeChanger.themeChanger.set_items(*themes)
         self.themeChanger.show()
